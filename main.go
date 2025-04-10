83853fc0327e6eecead18d6e247ab6fedf4d8142
Coșer Daneș <84979536+Danesinhell@users.noreply.github.com>
2025-04-11 00:42:52 +0300

    https instead of http

--- main.go
+++ main.go
@@ -42,6 +42,6 @@ func handleLocationPost(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/location", handleLocationPost)
-	fmt.Println("Server listening on http://localhost:8080")
+	fmt.Println("Server listening on https://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
